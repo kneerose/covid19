7ffbde6bbed25647c838546e341543b9761a2ba8
Niraj DUWAL <duwalkneerose@outlook.com>
2021-10-28 17:35:55 +0545

    Changed container to card

--- lib/component/piechart.dart
+++ lib/component/piechart.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
+
 // ignore: must_be_immutable
 class Piechart extends StatelessWidget {
-  double cases=0,deaths=0,active=0,recovered=0,tests=0;
-  String country="";
-  Color bgcolor = Colors.white;
-  Piechart({Key key,this.cases,this.deaths,this.active,this.recovered,this.tests,this.country,this.bgcolor}):super(key: key);
+  double cases = 0, deaths = 0, active = 0, recovered = 0, tests = 0;
+  String country = "";
+  Color bgcolor = Colors.white54;
+  Piechart(
+      {Key key,
+      this.cases,
+      this.deaths,
+      this.active,
+      this.recovered,
+      this.tests,
+      this.country,
+      this.bgcolor})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
@@ -14,39 +24,52 @@ class Piechart extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
+          Card(
             margin: const EdgeInsets.all(10),
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(8),border: Border.all(color:Colors.black),color: Colors.white),
+            shape: RoundedRectangleBorder(
+                side: BorderSide(
+              color: Colors.grey.withOpacity(0.4),
+              width: 0.5,
+            )),
+            //decoration: BoxDecoration(borderRadius: BorderRadius.circular(8),border: Border.all(color:Colors.black),color: Colors.white),
             child: Column(
-            children: [
-               Container(
-            margin: const EdgeInsets.only(top:15,left: 5,right: 5,bottom: 20),
-            child:Text("Pie chart of "+ country + " Covid19 datas",style: TextStyle(fontSize: 18))),
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: PieChart(dataMap: { "Deaths": deaths,
-                              "Active": active,
-                              "Cases": cases,
-                              "Recovered": recovered,
-              },
-              colorList: [Colors.red,Colors.grey,Colors.blue,Colors.green],
-              animationDuration:Duration(milliseconds: 800) ,
-                chartRadius: height/3,
-                               chartType: ChartType.disc,
-                               chartLegendSpacing: 20,
-                               chartValuesOptions: ChartValuesOptions(
-                                showChartValueBackground: true,
-                                showChartValues: true,
-                                showChartValuesInPercentage: true,
-                                showChartValuesOutside: false,
-                                decimalPlaces: 1,
-      ),
-              ),
-            )
-
-            ],
-          ),)
-         
+              children: [
+                Container(
+                    margin: const EdgeInsets.only(
+                        top: 15, left: 5, right: 5, bottom: 20),
+                    child: Text("Pie chart of " + country + " Covid19 datas",
+                        style: TextStyle(fontSize: 18))),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  child: PieChart(
+                    dataMap: {
+                      "Deaths": deaths,
+                      "Active": active,
+                      "Cases": cases,
+                      "Recovered": recovered,
+                    },
+                    colorList: [
+                      Colors.red,
+                      Colors.grey,
+                      Colors.blue,
+                      Colors.green
+                    ],
+                    animationDuration: Duration(milliseconds: 800),
+                    chartRadius: height / 3,
+                    chartType: ChartType.disc,
+                    chartLegendSpacing: 20,
+                    chartValuesOptions: ChartValuesOptions(
+                      showChartValueBackground: true,
+                      showChartValues: true,
+                      showChartValuesInPercentage: true,
+                      showChartValuesOutside: false,
+                      decimalPlaces: 1,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
