56d105a7391c3cdf1e71be959950436e420c71af
Niraj DUWAL <duwalkneerose@outlook.com>
2021-10-28 17:30:04 +0545

    Added themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
-import 'package:covid19/display.dart';
+import 'package:covid19/screen/display.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main()=>runApp(Home());
+void main() => runApp(Home());
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Display(),
+      home: Display(),
     );
   }
 }
---
+++ lib/themes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color barcolor = Color(0xff24405c);
+Color bgcolor = Colors.white;
+Color darkmodeba = Colors.blueGrey;
+Color darkmodeb = Color(0xa9a9a9);
+Color textcolor = Colors.black;
